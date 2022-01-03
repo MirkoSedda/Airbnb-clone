7acbe200158e0027b8773cccd1a3c48a8108579b
yourusername <kunay69@hotmail.it>
2022-01-03 21:01:41 +0100

    fixed randomising the indexes

--- index.js
+++ index.js
@@ -114,50 +114,50 @@ const shuffledCities = cities => {
 shuffledCities(cities)
 
 const randomizeOne = () => {
-  for (let i = 1; i <= 4; i++) {
-    randomOne.innerHTML += ` 
-  <div class="col">
-  <div class="d-flex align-items-center">
-    <img
-      src="/assets/homepage/e${i}.jpg"
-      class="rounded-3 selected-img"
-      id="img${i}"
-      alt="city picture"
-      width="100px"
-    />
-    <div class="ms-3">
-      <h5 class="text-bold fs-5">Milan</h5>
-      <span class="text-muted fs-6">1-hour drive</span>
+  let arr = [1, 2, 3, 4].sort(() => Math.random() - 0.5)
+  for (let i = 0; i <= 3; i++) {
+    randomOne.innerHTML += `
+    <div class="col">
+    <div class="d-flex align-items-center">
+      <img
+        src="/assets/homepage/e${arr[i]}.jpg"
+        class="rounded-3 selected-img"
+        id="img${arr[i]}"
+        alt="city picture"
+        width="100px"
+      />
+      <div class="ms-3">
+        <h5 class="text-bold fs-5">Milan</h5>
+        <span class="text-muted fs-6">1-hour drive</span>
+      </div>
     </div>
   </div>
-</div>
-  `
+    `
   }
 }
 randomizeOne()
 
 const randomizeTwo = () => {
   let arr = [1, 2, 3, 4].sort(() => Math.random() - 0.5)
-  console.log(arr)
-  // for (let i = 1; i <= 4; i++) {
-  //   randomTwo.innerHTML += `
-  //     <div class="col">
-  //     <div class="d-flex align-items-center">
-  //       <img
-  //         src="/assets/homepage/e${arr[i]}.jpg"
-  //         class="rounded-3 selected-img"
-  //         id="img${arr[i]}"
-  //         alt="city picture"
-  //         width="100px"
-  //       />
-  //       <div class="ms-3">
-  //         <h5 class="text-bold fs-5">Milan</h5>
-  //         <span class="text-muted fs-6">1-hour drive</span>
-  //       </div>
-  //     </div>
-  //   </div>
-  //     `
-  // }
+  for (let i = 0; i <= 3; i++) {
+    randomTwo.innerHTML += `
+    <div class="col">
+    <div class="d-flex align-items-center">
+      <img
+        src="/assets/homepage/e${arr[i]}.jpg"
+        class="rounded-3 selected-img"
+        id="img${arr[i]}"
+        alt="city picture"
+        width="100px"
+      />
+      <div class="ms-3">
+        <h5 class="text-bold fs-5">Milan</h5>
+        <span class="text-muted fs-6">1-hour drive</span>
+      </div>
+    </div>
+  </div>
+    `
+  }
 }
 randomizeTwo()
 
